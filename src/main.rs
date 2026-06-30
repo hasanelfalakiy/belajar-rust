a2a5791bf161233c5a8cd7b416e3bbe63ab005ae
hasanelfalakiy <andihasanelfalakiy@gmail.com>
2026-06-30 10:23:21 +0000

    variable argument parameter macro

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,22 @@ fn main() {
 #[test]
 fn variable() {
     let nama_variable = "predifined value ";
-	print!("{}", nama_variable);
+	println!("{}", nama_variable);
+	
+	let mut message_number = 1;
+	let message1 = "hello";
+	println!("message number {}: {}", message_number, message1);
+	
+	message_number = 2; // boleh diubah jika variable dideklarasikan sebagai mutable dengan keyword mut
+	let message2 = "world";
+	println!("message number {}: {}", message_number, message2);
+	
+	// argument parameter macro
+	message_number = 3;
+	let message3: i8 = 24;
+	/* terlihat seperti terbalik tapi lihat argumen indeks yang 
+	*  dimasukkan ke parameter macro di set ke 1 dulu baru ke 0
+	*/
+	println!("message number {1}: {0}", message3, message_number);
 }
+
