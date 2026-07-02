76f4ce89775dd57ebd81a132870c52d354ff91ac
hasanelfalakiy <andihasanelfalakiy@gmail.com>
2026-07-02 08:57:33 +0700

    komentar

--- src/pzn/hello_world.rs
+++ src/pzn/hello_world.rs
@@ -1,6 +1,14 @@
 #[cfg(test)]
 mod hello_test {
-	
+
+	// ini komentar satu baris
+
+	/*
+	ini komentar
+	lebih dari
+	satu baris
+	/ multi baris
+	 */
 	#[test]
 	fn hello_world() {
 		println!("hello rust form pzn!");
