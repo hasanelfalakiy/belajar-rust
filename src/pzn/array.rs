6eb9521b96e169b26f4d450d82adcec4347c45d1
hasanelfalakiy <andihasanelfalakiy@gmail.com>
2026-07-04 14:00:03 +0000

    array, persiapan array didalam array

--- src/pzn/array.rs
+++ src/pzn/array.rs
@@ -9,9 +9,41 @@ mod array_test {
 	#[test]
 	fn array() {
 	    // jika mau disebut secara eksplisit
-		// let angka: [i32, 5] = [1, 2, 3, 4, 5];
+		// let angka: [i32; 5] = [1, 2, 3, 4, 5];
 		let angka = [1, 2, 3, 4, 5];
 		
 		println!("{:?}", angka);
+		
+		// cara mengakses array dengan [indeks], indeks dimulai dari 0
+		let a = angka[0];
+		let b = angka[1];
+		println!("{} {}", a, b);
+		
+		/* 
+		sama seperti tuple, jika variable array nya dibuat immutable
+		maka isi data array tidak dapat diubah, jika ingin bisa diubah maka harus dibuat mutable
+		, untuk mengubah isi data array dengan [indeks] = nilai baru
+		*/
+		let mut num = [1, 2, 3, 4, 5];
+		
+		num[0] = 10;
+		num[1] = 20;
+		println!("{:?}", num);
+		
+		/*
+		Panjang Array
+		yg membedakan dengan tuple, di array kita bisa mendapatkan panjang array
+		dengan function bawaan array len() 
+		*/
+		let p = num.len();
+		println!("{}", p);
+		
+		/*
+		Dimensional Array
+		saat membuat array, kita bisa menggunakan tipe data apapun
+		termasuk array itu sendiri (array didalam array)
+		*/
+		
+		
 	}
 }
