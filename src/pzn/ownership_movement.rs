8325e51a4f7286902b7e6721417c19e983ec6c98
hasanelfalakiy <andihasanelfalakiy@gmail.com>
2026-07-06 09:38:55 +0700

    clone

--- src/pzn/ownership_movement.rs
+++ src/pzn/ownership_movement.rs
@@ -12,4 +12,14 @@ mod ownership_movement_test {
         // println!("{}", name1); name1 tidak bisa diakses
         println!("{}", name2);
     }
+
+    #[test]
+    fn clone() {
+        let name1 = String::from("Andi");
+        println!("{}", name1);
+
+        // kalau hanya let name2 = name1; saja maka pindah kepemilikan
+        let name2 = name1.clone(); // tapi clone itu berat
+        println!("{} {}", name1, name2);
+    }
 }
