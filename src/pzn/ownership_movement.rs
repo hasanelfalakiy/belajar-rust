f779f60fed9a3934d8c5f7140932de4dcd8928f7
hasanelfalakiy <andihasanelfalakiy@gmail.com>
2026-07-06 09:14:03 +0700

    ownership movement

--- src/pzn/ownership_movement.rs
+++ src/pzn/ownership_movement.rs
@@ -3,6 +3,7 @@ mod ownership_movement_test {
     #[test]
     fn ownership_movement() {
         let name1 = String::from("Andi");
+        println!("{}", name1);
 
         // ownership dari name1 dipindahkan ke name2
         let name2 = name1;
